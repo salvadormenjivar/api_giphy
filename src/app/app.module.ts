1301f2a34d48beff192f3e5c784c997c54ec2a1b
Salvador <salvadorpm@gmail.com>
2023-03-01 08:30:52 -0600

    Microservicio funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BusquedaModule } from './busqueda/busqueda.module';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     SharedModule,
-    BusquedaModule
+    BusquedaModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
